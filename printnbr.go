34213f635817921e9cb6417137fb5b7940aa29d0
Yerassyl Aldabergen <46417420+Era12@users.noreply.github.com>
2019-10-17 14:01:37 +0600

    Add files via upload

---
+++ printnbr.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintRune(n int) {
+
+	t := 1
+	if n < 0 {
+		t = -1
+		z01.PrintRune('-')
+	}
+	if n != 0 {
+		f := (n / 10) * t
+		if f != 0 {
+			PrintRune(f)
+		}
+		k := (n % 10 * t) + '0'
+		z01.PrintRune(Rune(k))
+	} else {
+		z01.PrintRune('0')
+	}
+
+}
